d1a4b7853371742ad7b5fd978359acc6686cf893
Gabriel Nakata <24640214+gabrielfnakata@users.noreply.github.com>
2025-03-11 22:15:02 -0300

    feat: adicionado atividades 1 e 2

---
+++ Atividade_2.js
@@ -0,0 +1,14 @@
+function mostraXadrez(num) { 
+    let output = ""; 
+    for(let i = 0; i < num; i++) { 
+        output += "# "; 
+    } 
+    for (let i = 0; i < num; i++) { 
+        if (i % 2 == 1) 
+            console.log(output); 
+        else 
+            console.log(" " + output); 
+    } 
+}
+
+mostraXadrez(8);
